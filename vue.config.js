5ea0e948edb01e4ddcd6d6e9ee92bb4833fd36e5
fengjw <2849425293@qq.com>
2022-08-23 14:41:55 +0800

    build: 项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置
+    } else {
+      return {
+        resolve: {
+          alias: {
+            '@js': path.resolve(__dirname, './src/assets/js'),
+            '@css': path.resolve(__dirname, './src/assets/css'),
+            '@img': path.resolve(__dirname, './src/assets/imgs'),
+            '@c': path.resolve(__dirname, './src/components')
+          }
+        }
+      }
+    }
+  }
 })
